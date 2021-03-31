4085d9258e1dcabebc744f7b0bf656b9f54c6f03
Greg-Beaucaire <greg.beaucaire@gmail.com>
2021-03-31 13:56:32 +0200

    ajout navbar view header et des switch case index

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+//bienvenue dans spaghetti-land YAY
+parse_str($_SERVER["QUERY_STRING"], $qs);
+$keys = array_keys($qs); // grab 'em all, if any
+$route = array_shift($keys); // first one
+
+  switch ($route){
+    case "genres":
+      include("src/php/controller/control.genres.php");
+    break;
+
+    case "styles":
+      include("src/php/controller/control.styles.php");
+    break;
+
+    case "artists":
+      include("src/php/controller/control.artists.php");
+    break;
+
+    default:
+      include("src/php/controller/control.select.php");
+    break;
+  }
+
+  exit;
--- src/php/view/view.header.php
+++ src/php/view/view.header.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <!-- Title -->
-    <meta name="description" content="Cameron Findlay vous propose un catalogue de musique">
-    <title>Cameron Findlay</title>
-    <meta charset="UTF-8"/>
-    <!-- Robots -->
-    <meta name="robots" content="index, follow">
-    <!-- Device -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="format-detection" content="telephone=no">
-    <!-- Links -->
-    <link rel="stylesheet" type="text/css" href="src/css/style.css"/>
+  <!-- Title -->
+  <meta name="description" content="Cameron Findlay vous propose un catalogue de musique">
+  <title>Cameron Findlay</title>
+  <meta charset="UTF-8" />
+  <!-- Robots -->
+  <meta name="robots" content="index, follow">
+  <!-- Device -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no">
+  <!-- Links -->
+  <link rel="stylesheet" type="text/css" href="src/css/style.css" />
 </head>
+
 <body>
+  <nav>
+    <h1>Cameron Findlay</h1>
+    <div class="menu">
+      <a href="?genres">genres</a>
+      <a href="?styles">styles</a>
+      <a href="?artists">artists</a>
+    </div>
+  </nav>
