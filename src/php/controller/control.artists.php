00a193167e3d586b569a6d75908145f79cdeb92d
Greg-Beaucaire <greg.beaucaire@gmail.com>
2021-03-31 14:38:45 +0200

    ajout des vues header/footer dans les différents controllers

---
+++ src/php/controller/control.artists.php
@@ -0,0 +1,18 @@
+<?php
+
+//MODEL
+  //DB
+
+  //Model Content
+  
+  //Fin Model Content
+
+
+//VIEW
+require "src/php/view/view.header.php";
+  // Content
+  
+  //End Content
+require "src/php/view/view.footer.php";
+
+?>
